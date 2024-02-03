81cc69fa2bee68bba8a81add9f0d27c9bf922013
m ad <mauricioadb@gmail.com>
2024-02-03 15:44:59 -0400

    fixed file stucture

--- spotify_playlist_extractor/app/config.py
+++
@@ -1,4 +0,0 @@
-CLIENT_ID = 'e9592dbb957d49ee9ce321f6d0104ac8'
-CLIENT_SECRET = '82f17282aa4b47ecbdb25f4be2b47890'
-REDIRECT_URI = 'http://127.0.0.1:5000'
-SCOPES = 'playlist-read-private'
--- spotify_playlist_extractor/app/spotify_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-from config import CLIENT_ID, CLIENT_SECRET
-
-def fetch_playlist_data(access_token, playlist_id):
-    headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=headers)
-    return response.json()  # Placeholder for actual processing
